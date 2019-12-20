dc1813c5bfa0311638b287c9fa35ef372779d1c8
Miles Butler <mtmbutler@icloud.com>
2019-12-19 20:24:45 -0800

    Write a test case struct and interface for easy endpoint unit testing

--- server_test.go
+++ server_test.go
@@ -8,33 +8,58 @@ import (
 	"testing"
 )
 
+type Case struct {
+	endpoint        string
+	reqType         string
+	body            []byte
+	params          map[string]string
+	expectedCode    int
+	expectedBodyStr string
+	serverFunc      http.HandlerFunc
+}
+
+func (c Case) run(t *testing.T) {
+	// Set up the request
+	req, err := http.NewRequest(c.reqType, c.endpoint, bytes.NewBuffer(c.body))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add GET parameters
+	q := req.URL.Query()
+	for k, v := range c.params {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	// Set up the handler
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(c.serverFunc)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != c.expectedCode {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, c.expectedCode)
+	}
+
+	// Check the response body is what we expect.
+	if rr.Body.String() != c.expectedBodyStr {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), c.expectedBodyStr)
+	}
+}
+
 func TestReturnAllUsers(t *testing.T) {
 	connectDB()
 	defer DB.Close()
 
-	jsonStr := []byte(``)
-
-	// Set up the request
-	req, err := http.NewRequest("GET", "/users", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		t.Fatal(err)
+	c := Case{
+		"/users",
+		"GET",
+		[]byte(``),
+		map[string]string{},
+		http.StatusUnauthorized,
+		``,
+		returnAllUsers,
 	}
-	// q := req.URL.Query()
-	// q.Add("apiKey", os.Getenv("ADMIN_KEY"))
-	// req.URL.RawQuery = q.Encode()
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(returnAllUsers)
-	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status != http.StatusUnauthorized {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusUnauthorized)
-	}
-
-	// Check the response body is what we expect.
-	expected := ``
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
+	c.run(t)
 }
